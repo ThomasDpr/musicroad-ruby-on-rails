a9d52d40480819087639bbb6a4cb96fc4b4d5b1f
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-07 17:02:11 +0200

    update index

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -3,9 +3,9 @@ class FestivalsController < ApplicationController
 
   def index
     @festivals = Festival.all
-    params[:query].present? ? @festivals = @festivals.where("title ILIKE ?", "%#{params[:query]}%") : @festivals
+    params[:query].present? ? @festivals = @festivals.where("name ILIKE ?", "%#{params[:query]}%") : @festivals
   end
-   
+
   def show
     @festival = Festival.find(params[:id])
     @artists = @festival.artists
