7dcd8ee5c7fcaf26e52dc0522f25914ca728e5bb
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-12 17:47:43 +0200

    Add filter date

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,18 +3,22 @@ import flatpickr from "flatpickr";
 import { French } from "flatpickr/dist/l10n/fr.js"
 
 export default class extends Controller {
+
+  static targets = ["flatpicker"]
+
   connect() {
     flatpickr(this.element, {
       locale: French,
       mode: "range",
-      dateFormat: "Y-m-d",
-      onChange: this.onChange.bind(this)
+      dateFormat: "d/m/Y",
+      onChange: this.onChange.bind(this),
     });
   }
 
   onChange(selectedDates, dateStr, instance) {
-    let [start_date, end_date] = dateStr.split(" to ");
+    let [start_date, end_date] = dateStr.split(" au ");
     this.element.querySelector('input[name="festival_filter[start_time]"]').value = start_date || "";
     this.element.querySelector('input[name="festival_filter[end_time]"]').value = end_date || "";
+    this.flatpickerTarget.value = dateStr;
   }
 }
