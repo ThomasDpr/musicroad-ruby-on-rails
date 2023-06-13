50f90aa34b49814ac2ea00ab8f6c709c6e304aa9
gonzague beyeler <gonzagubeyeler@gmail.com>
2023-06-13 08:16:22 +0200

    update stimulus

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -2,21 +2,51 @@ import { Controller } from "@hotwired/stimulus"
 import { createConsumer } from "@rails/actioncable"
 
 
-// Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
 
   static values = { chatroomId: Number }
   static targets = ["messages"]
+
   connect() {
+    console.log('hello')
     this.channel = createConsumer().subscriptions.create(
-      { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => this.#insertMessageAndScrollDown(data) },
-     ),
-     insertMessageAndScrollDown(data); {
-      this.messagesTarget.insertAdjacentHTML("beforeend", data)
+       { channel: "ChatroomChannel", id: this.chatroomIdValue },
+       { received: data => this.#insertMessageAndScrollDown(data) },
+      )
+  }
+
+  #insertMessageAndScrollDown(data) {
+      const currentUserIsSender = this.currentUserIdValue === data.sender_id;
+      const messageElement = this.#buildMessageElement(
+      currentUserIsSender,
+      data.message);
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
       this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+   }
+
+    //#buildMessageElement(currentUserIsSender, message) {
+        return `
+          <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+            <div class="${this.#userStyleClass(currentUserIsSender)}">
+              ${message}
+            </div>
+          </div>
+        `
       }
 
-      console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
-  }
+    #justifyClass(currentUserIsSender) {
+        return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+      }
+
+    #userStyleClass(currentUserIsSender) {
+      return currentUserIsSender ? "sender-style" : "receiver-style"
+      }
+      resetForm(event){
+      event.target.reset()
+      }
+
+      disconnect() {
+        console.log("Unsubscribed from the chatbox");
+        this.channel.unsubscribe();
+      }
 }
