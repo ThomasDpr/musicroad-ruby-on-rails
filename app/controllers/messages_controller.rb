934244e0fe7434cee2916e2435bc3f3daaf679d3
Elowan Audouin-le Guyader <elowan.aud@proton.me>
2023-06-13 15:42:22 +0200

    finish chatbox

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
+    @chatrooms = Chatroom.where(festival: @chatroom.festival)
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
@@ -15,7 +16,9 @@ class MessagesController < ApplicationController
       render "chatrooms/show", status: :unprocessable_entity
     end
   end
+
   private
+
   def message_params
     params.require(:message).permit(:content)
   end
