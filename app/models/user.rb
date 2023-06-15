b492ab5a249306f6407d1b8fc6a7c12fba7fb9f1
Thomas Dupre <thomasdpr-pro@outlook.com>
2023-06-15 12:24:49 +0200

    Create nil avatar profil with condition

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
+require 'open-uri'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :photo
+  after_create :assign_default_photo
+
+  def assign_default_photo
+    # raise
+    unless photo.attached?
+      file = URI.open('https://res.cloudinary.com/dimfha9hq/image/upload/v1686824175/ckbegohguqopibywv5uq.svg')
+      photo.attach(io: file, filename: 'avatar-login.png', content_type: 'image/svg')
+    end
+  end
 end
